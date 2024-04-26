1fa946f5095afd9c4b1b6e4fbc33ca320c86fa38
unknown <dieynabahamadymarigo2000@example.com>
2024-04-26 10:45:55 +0000

    second commit

---
+++ src/app/components/cv-details/cv-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-details',
+  templateUrl: './cv-details.component.html',
+  styleUrl: './cv-details.component.css'
+})
+export class CvDetailsComponent {
+
+}
