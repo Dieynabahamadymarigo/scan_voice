bfd92d38f8610e269664df686b432e7757b72213
fatimaissa789 <mamefatimandiaye87@gmail.com>
2024-06-10 12:11:15 +0000

    tester

--- sql - Copie/routes/scan.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const scanController = require('../controllers/scan');
-
-const router = express.Router();
-
-router.get('/scan', scanController.scan)
-
-module.exports = router;
